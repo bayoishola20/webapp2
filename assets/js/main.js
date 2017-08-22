6f570703c1de4571b92d8bbbc06823e6a9b897c5
bayoishola20 <bayoishola20@yahoo.com>
2017-08-22 18:48:04 +0100

    Attribute change

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,18 @@ require([
       "esri/widgets/BasemapToggle",
       "esri/widgets/ScaleBar",
 
+      //Routes
+      "esri/Graphic",
+      "esri/layers/GraphicsLayer",
+      "esri/tasks/RouteTask",
+      "esri/tasks/support/RouteParameters",
+      "esri/tasks/support/FeatureSet",
+      "esri/symbols/SimpleMarkerSymbol",
+      "esri/symbols/SimpleLineSymbol",
+      "esri/Color",
+      "esri/core/urlUtils",
+      "dojo/on",
+
       // Bootstrap
       "bootstrap/Collapse",
       "bootstrap/Dropdown",
@@ -20,13 +32,51 @@ require([
       "calcite-maps/calcitemaps-v0.4",
       
       "dojo/domReady!"
-    ], function(WebMap, MapView, Basemaps, Search, Legend, LayerList, Print, BasemapToggle, ScaleBar) {
+    ], function(WebMap, MapView, Basemaps, Search, Legend, LayerList, Print, BasemapToggle, ScaleBar, Graphic, GraphicsLayer, RouteTask, RouteParameters,
+      FeatureSet, SimpleMarkerSymbol, SimpleLineSymbol, Color, urlUtils, on) {
 
       /******************************************************************
        *
        * Create the map, view and widgets
        * 
        ******************************************************************/
+      
+      // Proxy the route requests to avoid prompt for log in
+      urlUtils.addProxyRule({
+        urlPrefix: "route.arcgis.com",
+        proxyUrl: "/sproxy/"
+      });
+
+      // Point the URL to a valid route service
+      var routeTask = new RouteTask({
+        url: "https://route.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World"
+      });
+
+      // The stops and route result will be stored in this layer
+      var routeLyr = new GraphicsLayer();
+
+      // Setup the route parameters
+      var routeParams = new RouteParameters({
+        stops: new FeatureSet(),
+        outSpatialReference: { // autocasts as new SpatialReference()
+          wkid: 3857
+        }
+      });
+
+      // Define the symbology used to display the stops
+      var stopSymbol = new SimpleMarkerSymbol({
+        style: "cross",
+        size: 15,
+        outline: { // autocasts as new SimpleLineSymbol()
+          width: 4
+        }
+      });
+
+      // Define the symbology used to display the route
+      var routeSymbol = new SimpleLineSymbol({
+        color: [0, 0, 255, 0.5],
+        width: 5
+      });
 
       // Map
       var map = new WebMap({
@@ -35,7 +85,7 @@ require([
           // id: "84d09c8b6551486ea4ecec6c2979ca0e" //hosted, will require login
           id: "2167063b86bc4288a39e5c4c844a67b2" //map layer item
           // id: "00c54fece73a47e8a3d6b535d21997c8" // raw shapefile but requires login
-        }
+        },
       });
       
       // View
@@ -47,8 +97,32 @@ require([
           bottom: 0
         },
         center: [7.5030633, 9.0402785],
-        zoom: 8
+        zoom: 8,
       });
+
+      // Adds a graphic when the user clicks the map. If 2 or more points exist, route is solved.
+      on(mapView, "click", addStop);
+
+      function addStop(event) {
+        // Add a point at the location of the map click
+        var stop = new Graphic({
+          geometry: event.mapPoint,
+          symbol: stopSymbol
+        });
+        routeLyr.add(stop);
+
+        // Execute the route task if 2 or more stops are input
+        routeParams.stops.features.push(stop);
+        if (routeParams.stops.features.length >= 2) {
+          routeTask.solve(routeParams).then(showRoute);
+        }
+      }
+      // Adds the solved route to the map as a graphic
+      function showRoute(data) {
+        var routeResult = data.routeResults[0].route;
+        routeResult.symbol = routeSymbol;
+        routeLyr.add(routeResult);
+      }
 
       // Popup
       mapView.then(function(){
